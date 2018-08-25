6bdc625f281c8246eb9f5a071e03ae0f253f7495
Stefan Helmert <stefan.helmert@informatik.tu-chemnitz.de>
2018-08-25 21:53:46 +0200

    added AGPL header

--- backupall.bash
+++ backupall.bash
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+#    git-restore: Simple backup utility for servers, no extra software on server needed
+#    Copyright (C) 2018  Stefan Helmert <stefan.helmert@t-online.de>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 tag="backup-$(date +%Y%m%d%k%M%S)"
